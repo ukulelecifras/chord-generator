67040f2deea0995277662e591caa17c8e6f8d8f4
Thiago Colares <thicolares@gmail.com>
2018-12-23 09:56:30 -0300

    Add a stub class with license header

---
+++ src/Model/UkuleleFretboard.php
@@ -0,0 +1,18 @@
+<?php
+namespace ChordGenerator\Model;
+
+/**
+ * This file is part of the PhpStorm.
+ *
+ * Created by Thiago Colares
+ *
+ * (c) Contributors of the Ukulele Cifras project - www.ukulelecifras.com.br
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+class UkuleleFretboard
+{
+
+}
